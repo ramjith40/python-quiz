1d695e3df0d608213e281766a6dc409ccb1535df
ramjith40 <rabetkko@gmail.com>
2026-02-03 23:20:18 +0530

    fix cors

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-CORS(app)
 import random
 import time
 
 app = Flask(__name__)
+CORS(app)
 
 # 🔥 IMPORTANT: allow all origins
 CORS(app, resources={r"/*": {"origins": "*"}})
